8f9613c867bbcd86d62e4f693ca39264a6945a38
tuyenleTX <ttle@miu.edu>
2025-10-23 09:17:04 -0500

    updated test26

--- tests/Day26.spec.ts
+++ tests/Day26.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import { TIMEOUT } from "../constants/timeout";
 //Explicit wait
 //const CUSTOM_TIMEOUT = { timeout: 15 * 1000 };
@@ -51,4 +51,33 @@ test(`Link text - multimatching`, async ({ page }) => {
 
     //debug purpose only
     await page.waitForTimeout(3 * 1000);
+})
+
+test('Form Authentication', async ({ page }) => {
+    const USERNAME_SEL = "#username";
+    const PASSWORD_SEL = "#password";
+    const LOGIN_CRED = { username: "tomsmith", password: "SuperSecretPassword!" };
+    const LOGIN_SEL = "button[type='submit']";
+    const DASHBOARD_HEADING_SEL = 'h2';
+    const EXPECTED_DASHBOARD_HEADING_TEXT = 'Secure Area';
+
+    // Go to homepage
+    await page.goto('/');
+    // Navigate to the authentication page
+    await page.locator("a").filter({ hasText: 'Form Authentication' }).click();
+
+    await page.locator(USERNAME_SEL).fill(LOGIN_CRED.username);
+    await page.locator(PASSWORD_SEL).fill(LOGIN_CRED.password);
+    // Submit the form
+    await page.locator(LOGIN_SEL).click();
+
+    // Get the heading text on dashboard page
+    const dashboardHeadingText = await page.locator(DASHBOARD_HEADING_SEL).textContent();
+    console.log(dashboardHeadingText?.trim());
+
+    const dashboardHeadingInnerText = await page.locator(DASHBOARD_HEADING_SEL).innerText();
+    console.log(dashboardHeadingInnerText);
+
+    expect(dashboardHeadingInnerText).toBe(EXPECTED_DASHBOARD_HEADING_TEXT);
+
 })
